ef15fb7bd2154d66f9b4a6496642fc5ee4e7955a
Oscar249 <80767588+Oscar249@users.noreply.github.com>
2021-06-15 16:01:01 -0500

    Create chapter04.ex05(4-5)-Oscar Romo.c

---
+++ chapter04.ex05(4-5)-Oscar Romo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+  /*Print floating-point number using the %d conversion*/
+float fp;
+fp = 7.79;
+
+printf("Float is %d\n", float);
+
+  //Print integer using the %f conversion
+int inte;
+inte = 4;
+
+printf("Integer is %f\n");
+
+  //Print character using the %d conversion
+
+char cha ='a';
+printf("Character is %d", cha);
+
+}
