bc505a97ff3f2658fea3750fb2b969051354078c
Oscar249 <80767588+Oscar249@users.noreply.github.com>
2021-06-15 15:35:11 -0500

    Create chapter04.ex01(4-1)-Oscar Romo.c

---
+++ chapter04.ex01(4-1)-Oscar Romo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void){ 
+/*In this program you can print your name, date of birth and social security number*/
+    printf("Name: Oscar Romo\n");
+    printf("social security number: 5616021920\n");
+    printf("Date of birth: 24/february/2002\n");
+ return 0;
+
+  }
