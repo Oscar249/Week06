33fef9ee3df46c93bdbbbf67d816138ea3bb3653
Oscar249 <80767588+Oscar249@users.noreply.github.com>
2021-06-15 18:50:38 -0500

    Update chapter05.ex01(5-1)-Oscar Romo.c

--- chapter05.ex01(5-1)-Oscar Romo.c
+++ chapter05.ex01(5-1)-Oscar Romo.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//this one converts centigrades to farenheit, c=centigrades, f=farenheit
+/*in this command we covert centigrades in farenheit (c=centigrades,f=farenheit*/
 float c ;
 float f; 
 int main(void){
@@ -7,7 +7,7 @@ int main(void){
 
   printf("Enter centigrades\n");
   scanf("%f", &c);
-  //to the conversion i use f=9/5*c+32
+  /*to convert i we use f=9/5*c+32*/
   f= (c*9/5)+32;
 
   printf("the result of the operation is %.f", f);
